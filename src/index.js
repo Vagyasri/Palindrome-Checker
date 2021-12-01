95d6d25df4c3e8b28958d943da1e52faf3838786
Vagyasri <vagyasri2011@gmail.com>
2021-12-01 20:01:10 +0100

    Create function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './style.css';
+
+const palindrome = (str) => {
+  let arr = [];
+  const newStr = str.toLowerCase().split('');
+  for (let i = newStr.length - 1; i >= 0; i -= 1) {
+    arr.push(newStr[i]);
+  }
+  str = newStr.join('');
+  arr = arr.join('');
+  console.log(str);
+  console.log(arr);
+  return (arr === str);
+}
