e7bd39b075c245d8ad02ba4daa935c87cc338a13
Vagyasri <vagyasri2011@gmail.com>
2021-12-01 20:05:48 +0100

    Add test cases

--- src/index.js
+++ src/index.js
@@ -11,4 +11,17 @@ const palindrome = (str) => {
   console.log(str);
   console.log(arr);
   return (arr === str);
-}
+};
+
+console.log(palindrome('eye'));
+console.log(palindrome('_eye'));
+console.log(palindrome('race car'));
+console.log(palindrome('not a palindrome'));
+console.log(palindrome('A man, a plan, a canal. Panama'));
+console.log(palindrome('never odd or even'));
+console.log(palindrome('nope'));
+console.log(palindrome('almostomla'));
+console.log(palindrome('My age is 0, 0 si ega ym.'));
+console.log(palindrome('1 eye for of 1 eye.'));
+console.log(palindrome('0_0 (: /-\\ :) 0-0'));
+console.log(palindrome('five|\\_/|four'));
